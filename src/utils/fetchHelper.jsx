1d0bcc084fea0b7db66df8e8cbe0a6964efea709
Charly <carlosmaccarrone@gmail.com>
2025-08-17 14:00:07 -0300

    fix: rollback BrowserRouter to HashRouter

--- src/utils/fetchHelper.jsx
+++
@@ -1,7 +0,0 @@
-const BASE_PATH = window.location.hostname.includes('github.io') ? 'https://carlosmaccarrone.github.io/MacMovies' : '';
-
-export async function fetchJSON(path, options = {}) {
-  const response = await fetch(`${BASE_PATH}${path}`, options);
-  if (!response.ok) throw new Error(`Fetch error: ${response.status}`);
-  return response.json();
-}
